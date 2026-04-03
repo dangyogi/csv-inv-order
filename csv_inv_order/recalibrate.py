34d4bb8a2fa77112be3b1efd0b24a84e40606cdb
Steven Sorenson <matzoman2006@yahoo.com>
2026-04-03 12:20:25 -0400

    Did recalibrate, also changed Half & Half to Coffee Creamer

--- csv_inv_order/recalibrate.py
+++ csv_inv_order/recalibrate.py
@@ -8,7 +8,7 @@ from .database import *
 
 
 def get_counts():
-    delta = defaultdict(float)  # {item: delta}       added to second count
+    delta = defaultdict(float)      # {item: delta}       added to second count
     starting_dates = {}             # {item: date}        date of starting count
     starting_counts = {}            # {item: total_units}
     for row in Inventory.values():
@@ -37,7 +37,6 @@ def get_breakfast(date):
 def run():
     load_database()
     for item, counts in groupby(sorted(get_counts()), key=itemgetter(0)):
-        item_counts = list(counts)
         item_obj = Items[item]
         if item_obj.num_per_serving is not None:
             print(f"{item}: stored num_per_serving={item_obj.num_per_serving}")
@@ -47,19 +46,22 @@ def run():
             print(f"{item}: stored num_per_table={item_obj.num_per_table}")
         else:
             print(f"{item}: no stored consumption set")
-        for item, start_date, start_count, end_date, end_count in item_counts:
+        for item, start_date, start_count, end_date, end_count in counts:
             last_breakfast = get_breakfast(end_date)
             total_served = 0
+            total_months = 0
             month = get_breakfast(start_date)  # don't count this one!
             while True:
                 month = Months[Months.inc_month(month.year, month.month)]
                 if month.key() > last_breakfast.key():
                     break
                 total_served += month.meals_served
+                total_months += 1
             consumed = start_count - end_count
-            print(f"  {item=}, {total_served=}, {consumed=:.02f}", end='')
+            print(f"  {item=}, start={start_date:'%b %d, %y'}, end={end_date:'%b %d, %y'}, "
+                  f"served={total_served}, months={total_months}, {consumed=:.02f}", end='')
             if total_served:
-                print(f", num_per_serving={consumed/total_served:.02f}")
+                print(f", per_serving={consumed/total_served:.02f}")
             else:
                 print()
 
